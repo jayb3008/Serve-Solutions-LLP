aaffce6b305cab0e8cb633241f572d974b8bb0cb
jayb22 <jays22@groovyweb.co>
2024-04-17 23:36:52 +0530

    ADD:Website code

---
+++ src/components/Video/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import SectionTitle from "../Common/SectionTitle";
+
+import ModalVideo from "react-modal-video";
+import SingleFeature from "../Features/SingleFeature";
+import featuresData from "../Features/featuresData";
+
+const Video = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <section className="relative z-10 py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <SectionTitle
+          title="What we do"
+          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          center
+          mb="80px"
+        />
+
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div
+              className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-4"
+              data-wow-delay=".15s"
+            >
+              {/* <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3"> */}
+              {featuresData.map((feature) => (
+                <SingleFeature key={feature.id} feature={feature} />
+              ))}
+              {/* </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 left-0 right-0 z-[-1] h-full w-full bg-[url(/images/video/shape.svg)] bg-cover bg-no-repeat"></div>
+    </section>
+  );
+};
+
+export default Video;
