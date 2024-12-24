a4ccf23ed4e7f4e44738ce400b22f0b5ce59d95d
serve1solutions <167350408+serve1solutions@users.noreply.github.com>
2024-12-24 12:42:30 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Serve Solutions LLP",
   description: "This is Home for Startup Nextjs Template",
-  // other metadata
 };
 
 export default function Home() {
